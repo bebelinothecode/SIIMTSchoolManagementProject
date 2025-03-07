e87021161fd925a29c7583abce78d6653a4fc169
Isaac Dickson <bebelino45@gmail.com>
2025-03-07 02:44:51 -0800

    Twelve Updates

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user99 = User::create([
+            'name'          => 'Christabel Sowah',
+            'email'         => 'christabelsowah@demo.com',
+            'password'      => bcrypt('12345678'),
+            'created_at'    => date("Y-m-d H:i:s")
+        ]);
+        $user99->assignRole('frontdesk');
+
+        $user100 = User::create([
+            'name'          => 'Cindy Amartei',
+            'email'         => 'cindyamartei@demo.com',
+            'password'      => bcrypt('12345678'),
+            'created_at'    => date("Y-m-d H:i:s")
+        ]);
+        $user100->assignRole('frontdesk');
+        //
+    }
+}
